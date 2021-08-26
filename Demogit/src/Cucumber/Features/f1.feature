c25411d9261a8b811cf620486d24e8eb4328d5ab
mungle.priya <mungle.priya@LP-PF2WM1GZ.HCLT.CORP.HCL.IN>
2021-08-26 20:01:28 +0530

    committed

---
+++ Demogit/src/Cucumber/Features/f1.feature
@@ -0,0 +1,5 @@
+Feature:KSRTC application
+Scenario:KSTRC application to test registration page
+Given Open the chrome browser and url of the application
+When Enter the username and fullname and phone number
+Then Registered successfully
---
+++ Demogit/src/demo/Testcases.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Testcases   
+{  
+@Test  
+public void testtopass()  
+{  
+Assert.assertTrue(true);  
+}  
+@Test  
+public void testtofail()  
+{  
+Assert.assertFalse(false);  
+}  
+}  
